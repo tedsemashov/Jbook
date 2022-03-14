399226df2e2d71bb7e343fe1db914af43f2cd720
tedsemashov <ted.semashov@gmail.com>
2022-03-14 21:36:50 +0100

    local-client updates + build + modules

--- packages/local-client/src/index.tsx
+++ packages/local-client/src/index.tsx
@@ -9,7 +9,9 @@ const App = () => {
 
     return (
         <Provider store={store}>
-            <CellList />
+            <div>
+                <CellList />
+            </div>
         </Provider>
     )
 };
